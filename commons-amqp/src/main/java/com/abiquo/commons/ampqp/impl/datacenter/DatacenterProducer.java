825adc2bb50bf84ba5722731d76684a6f3f7e1a2
Enric Ruiz <morelies@gmail.com>
2011-02-21 15:28:58 +0100

    Added datacenter configuration and an empty dto.

---
+++ commons-amqp/src/main/java/com/abiquo/commons/ampqp/impl/datacenter/DatacenterProducer.java
@@ -0,0 +1,45 @@
+/**
+ * Abiquo community edition
+ * cloud management application for hybrid clouds
+ * Copyright (C) 2008-2010 - Abiquo Holdings S.L.
+ *
+ * This application is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC
+ * LICENSE as published by the Free Software Foundation under
+ * version 3 of the License
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * LESSER GENERAL PUBLIC LICENSE v.3 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+package com.abiquo.commons.ampqp.impl.datacenter;
+
+import static com.abiquo.commons.amqp.util.ProducerUtils.publishPersistentText;
+
+import java.io.IOException;
+
+import com.abiquo.commons.ampqp.impl.datacenter.domain.DatacenterJob;
+import com.abiquo.commons.amqp.producer.BasicProducer;
+
+public class DatacenterProducer extends BasicProducer<DatacenterConfiguration, DatacenterJob>
+{
+    @Override
+    public DatacenterConfiguration configurationInstance()
+    {
+        return DatacenterConfiguration.getInstance();
+    }
+
+    @Override
+    public void publish(DatacenterJob message) throws IOException
+    {
+        publishPersistentText(channel, DatacenterConfiguration.getExchangeName(),
+            DatacenterConfiguration.getRoutingKey(), message.toByteArray());
+    }
+}
