4ca7c4a0ddc44a483c952409ae2840fdbfcc8d0d
Enric Ruiz <morelies@gmail.com>
2011-03-30 18:10:56 +0200

    Runtime exception on invalid hypervisor

--- commons-amqp/src/main/java/com/abiquo/commons/amqp/impl/datacenter/domain/HypervisorConnection.java
+++ commons-amqp/src/main/java/com/abiquo/commons/amqp/impl/datacenter/domain/HypervisorConnection.java
@@ -128,6 +128,5 @@ public class HypervisorConnection
                     throw new RuntimeException("Unable to build the connection URI, unknown hypervisor.");
             }
         }
-
     }
 }
