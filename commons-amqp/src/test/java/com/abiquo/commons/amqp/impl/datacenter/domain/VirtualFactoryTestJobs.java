c2399e3a4fe068d7bff39293ab2c43aa3983c83c
Enric Ruiz <morelies@gmail.com>
2011-04-11 09:58:29 +0200

    Test fix.

--- commons-amqp/src/test/java/com/abiquo/commons/amqp/impl/datacenter/domain/VirtualFactoryTestJobs.java
+++ commons-amqp/src/test/java/com/abiquo/commons/amqp/impl/datacenter/domain/VirtualFactoryTestJobs.java
@@ -46,7 +46,7 @@ public class VirtualFactoryTestJobs
             // .primaryDisk("RAW", "1024", "iqn.bla.bla-lun-0")
             .primaryDisk(DiskFormatType.RAW, 1024l, "nfs-devel:/opt/vm_repo",
                 "1/rs.bcn/m0n0/m0n0.iso", "datastore1") //
-            .addAuxDisk(DiskFormatType.RAW, 1024l, "iqn....", 1);
+            .addAuxDisk(DiskFormatType.RAW, 1024l, "iqn....", "sdasd", 1);
 
     }
 
